8b62af4bcb5c6d91285af07532e03821448ea97e
poka <poka@p2p.legal>
2024-03-18 00:32:28 +0100

    wip03

--- assets/javascripts/discourse/controllers/add-event.js
+++ assets/javascripts/discourse/controllers/add-event.js
@@ -1,30 +1,55 @@
-import ModalFunctionality from "discourse/mixins/modal-functionality";
-import Controller from "@ember/controller";
 import I18n from "I18n";
 import { action } from "@ember/object";
+import Component from "@ember/component";
+import { inject as service } from "@ember/service";
 
-export default Controller.extend(ModalFunctionality, {
+export default Component.extend({
+  modal: service(),
+
   title: "add_event.modal_title",
 
-  @action
-  clear() {
-    event?.preventDefault();
-    this.set("bufferedEvent", null);
+  init() {
+    this._super(...arguments);
+    console.log('AddEvent component initialized');
   },
 
-  actions: {
-    saveEvent() {
-      if (this.valid) {
-        this.get("model.update")(this.bufferedEvent);
-        this.send("closeModal");
-      } else {
-        this.flash(I18n.t("add_event.error"), "error");
-      }
-    },
+  didInsertElement() {
+    this._super(...arguments);
+    console.log('AddEvent component inserted into the DOM');
+  },
 
-    updateEvent(event, valid) {
-      this.set("bufferedEvent", event);
-      this.set("valid", valid);
-    },
+  willDestroyElement() {
+    this._super(...arguments);
+    console.log('AddEvent component about to be removed from the DOM');
+  },
+
+  @action
+  clear(event) {
+    event?.preventDefault();
+    this.set("model.bufferedEvent", null);
+    console.log('AddEvent clear action triggered');
+  },
+
+  @action
+  saveEvent() {
+    console.log('AddEvent saveEvent action triggered');
+    if (this.model.valid) {
+      this.model.update(this.model.bufferedEvent);
+      this.modal.close();
+    } else {
+      this.flashMessages.add({
+        message: I18n.t("add_event.error"),
+        type: "error",
+      });
+    }
+  },
+
+  @action
+  updateEvent(event, valid) {
+    this.setProperties({
+      "model.bufferedEvent": event,
+      "model.valid": valid,
+    });
+    console.log('AddEvent updateEvent action triggered', event, valid);
   },
 });
